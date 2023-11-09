6cb7ec03abaa483ea0e57770602644327972997c
Peter Maczo <peter.maczo@gmail.com>
2023-11-09 17:52:09 +0100

    fe: components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,64 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios'
+import { Panel, Loading, Row, ListElement, ImagePreview, ImageLink } from './Components'
 import './App.css'
 const REST_API_URL = import.meta.env.VITE_REST_API_URL;
 const REPO_URL = import.meta.env.VITE_REPO_URL;
-
-function Panel({ children }: { children: React.ReactNode }) {
-  return <div className='panel'>
-    <div className='panel_content'>
-      {children}
-    </div>
-  </div>
-}
-
-function Loading() {
-  return <div className='spinner_container'>
-    <svg xmlns="http://www.w3.org/2000/svg" className="spinner" viewBox="0 0 50 50">
-      <circle className="path" cx="25" cy="25" r="20" fill="none" stroke-width="5" ></circle>
-    </svg>
-  </div>
-}
-
-function Row({ children, minHeight }: { children: React.ReactNode, minHeight?: number }) {
-  return <div className='row' style={{ minHeight }}>{children}</div>
-}
-
-function ListElement({ file, selectFile, currFile }: { file: string, selectFile: (file: string) => void, currFile: string }) {
-  return <div
-    className={'file' + (currFile === file ? ' selected' : '')}
-    onClick={() => selectFile(file)}
-  >
-    {file}
-  </div>
-}
-
-function ImagePreview({ url, onFileLoaded }: { url: string, onFileLoaded: VoidFunction }) {
-  const [src, setSrc] = useState("")
-
-  useEffect(() => {
-    setSrc("")
-    const image = new Image()
-    image.src = url
-    image.onload = () => {
-      onFileLoaded()
-      setSrc(url)
-    }
-  }, [url])
-
-  return <div className='image_container'>
-    {url && !src && <Loading />}
-    {src && <a href={src} target='_blank'><img src={src} alt={src} /></a>}
-  </div>
-
-}
-
-function ImageLink({ url }: { url: string }) {
-  return <div className='image_link' >
-    <textarea value={url} rows={10} disabled ></textarea>
-    <a className='cover' href={url} target="_blank"></a>
-  </div>
-}
 
 interface State {
   files: string[],
@@ -68,7 +13,7 @@ interface State {
   currLoading: boolean,
 }
 
-function App() {
+export const App = () => {
   const [state, _setState] = useState<State>({
     files: [],
     currFile: "",
@@ -132,5 +77,3 @@ function App() {
     </>
   )
 }
-
-export default App
---
+++ frontend/src/Components.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+
+export const Panel = ({ children }: { children: React.ReactNode }) => {
+  return <div className='panel'>
+    <div className='panel_content'>
+      {children}
+    </div>
+  </div>
+}
+
+export const Loading = () => {
+  return <div className='spinner_container'>
+    <svg xmlns="http://www.w3.org/2000/svg" className="spinner" viewBox="0 0 50 50">
+      <circle className="path" cx="25" cy="25" r="20" fill="none" stroke-width="5" ></circle>
+    </svg>
+  </div>
+}
+
+export const Row = ({ children, minHeight }: { children: React.ReactNode, minHeight?: number }) => {
+  return <div className='row' style={{ minHeight }}>{children}</div>
+}
+
+export const ListElement = ({ file, selectFile, currFile }: { file: string, selectFile: (file: string) => void, currFile: string }) => {
+  return <div
+    className={'file' + (currFile === file ? ' selected' : '')}
+    onClick={() => selectFile(file)}
+  >
+    {file}
+  </div>
+}
+
+export const ImagePreview = ({ url, onFileLoaded }: { url: string, onFileLoaded: VoidFunction }) => {
+  const [src, setSrc] = useState("")
+
+  useEffect(() => {
+    setSrc("")
+    const image = new Image()
+    image.src = url
+    image.onload = () => {
+      onFileLoaded()
+      setSrc(url)
+    }
+  }, [url])
+
+  return <div className='image_container'>
+    {url && !src && <Loading />}
+    {src && <a href={src} target='_blank'><img src={src} alt={src} /></a>}
+  </div>
+
+}
+
+export const ImageLink = ({ url }: { url: string }) => {
+  return <div className='image_link' >
+    <textarea value={url} rows={10} disabled ></textarea>
+    <a className='cover' href={url} target="_blank"></a>
+  </div>
+}
