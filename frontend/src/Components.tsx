0b47b0a2bdab3316e6a251eefe03b777771ecfef
Peter Maczo <peter.maczo@gmail.com>
2023-11-09 17:57:53 +0100

    fe: fix

--- frontend/src/Components.tsx
+++ frontend/src/Components.tsx
@@ -11,7 +11,7 @@ export const Panel = ({ children }: { children: React.ReactNode }) => {
 export const Loading = () => {
   return <div className='spinner_container'>
     <svg xmlns="http://www.w3.org/2000/svg" className="spinner" viewBox="0 0 50 50">
-      <circle className="path" cx="25" cy="25" r="20" fill="none" stroke-width="5" ></circle>
+      <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5" ></circle>
     </svg>
   </div>
 }
